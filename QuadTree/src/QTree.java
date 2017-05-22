8ec81b755d2181bd3336f082b6a4c51defcf9e14
chrislucas <christoffer.luccas@gmail.com>
2017-05-22 20:37:33 -0300

    tentando entender quadtree

--- QuadTree/src/QTree.java
+++ QuadTree/src/QTree.java
@@ -3,9 +3,42 @@
  */
 public class QTree {
 
+    //
+    public static class Point2D {
+        int x, y;
+        public Point2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static class Rect {
+        Point2D p1, p2;
+        public Rect(Point2D p1, Point2D p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public int minX() {
+            return p1.x < p2.x ? p1.x : p2.x;
+        }
+
+        public int minY() {
+            return p1.y < p2.y ? p1.y : p2.y;
+        }
+
+        public int maxX() {
+            return p1.x < p2.x ? p2.x : p1.x;
+        }
+
+        public int maxY() {
+            return p1.y < p2.y ? p2.y : p1.y;
+        }
+    }
+
     public static final class Node /* implements Comparable<Node> */ {
         private int x, y, w, h, value;
-        private Node n,s,l,o;   // cada node possui 4 subnodes
+        private Node nw, ne, sw, se;   // cada node possui 4 subnodes
         private Node parent;    // no pai
         public Node(int x, int y, int value) {
             this.x = x;
@@ -34,23 +67,34 @@ public class QTree {
             return createNode(x, y, value);
         }
         else if(less(x, node.x) && less(y, node.y)) {
-            node.s = insert(node.s, x, y, value);
+            node.sw = insert(node.sw, x, y, value);
         }
         else if(less(x, node.x) && !less(y, node.y)) {
-            node.n = insert(node.n, x, y, value);
+            node.nw = insert(node.nw, x, y, value);
         }
         else if(!less(x, node.x) && less(y, node.y)) {
-            node.l = insert(node.l, x, y, value);
+            node.se = insert(node.se, x, y, value);
         }
         else if(!less(x, node.x) && !less(y, node.y)) {
-            node.o = insert(node.o, x, y, value);
+            node.ne = insert(node.ne, x, y, value);
         }
         return node;
     }
 
-    private void query(Node node) {
-
+    private void query(Rect rect) {
+        query(root, rect);
     }
+
+    private void query(Node node, Rect rect) {
+        if(node == null)
+            return;
+        int minX = rect.minX();
+        int minY = rect.minY();
+        int maxX = rect.maxX();
+        int maxY = rect.maxY();
+    }
+
+
 
     public static void main(String[] args) {
 
