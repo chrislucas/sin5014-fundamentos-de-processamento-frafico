677878bdc11a0d7bde14725c67e576dfc2988cd0
chrislucas <christoffer.luccas@gmail.com>
2017-06-09 18:08:33 -0300

    commit

---
+++ HRChallenger/src/ExpADJMatrix.java
@@ -0,0 +1,11 @@
+/**
+ * Created by r028367 on 09/06/2017.
+ *
+ * https://en.wikipedia.org/wiki/Adjacency_matrix#Matrix_powers
+ * https://pt.wikipedia.org/wiki/Matriz_de_adjac%C3%AAncia
+ * https://en.wikipedia.org/wiki/Graph_power
+ * https://mathoverflow.net/questions/118016/power-of-adjacency-matrix
+ * https://mathoverflow.net/questions/65118/what-is-the-n-th-power-of-the-adjacency-matrix-equal-to
+ */
+public class ExpADJMatrix {
+}
---
+++ HRChallenger/src/PK.java
@@ -0,0 +1,7 @@
+/**
+ * Created by r028367 on 09/06/2017.
+ * https://www.hackerearth.com/pt-br/problem/algorithm/pk-and-interesting-language/description/
+ *
+ */
+public class PK {
+}
